269bef2a7b479b2a8f07f3e6d38aef98c9ca3b44
00000000 <a00000000@00000000s-MBP.attlocal.net>
2022-10-30 13:17:23 -0500

    Setup T5-base model

--- T5_summarization/train.py
+++ T5_summarization/train.py
@@ -5,8 +5,26 @@ from transformers import AutoModelForSeq2SeqLM, Seq2SeqTrainingArguments, Seq2Se
 from transformers import AutoModelForSeq2SeqLM, DataCollatorForSeq2Seq, Seq2SeqTrainingArguments, Seq2SeqTrainer
 from transformers import create_optimizer, AdamWeightDecay
 
+prefix = "summarize: "
+model_checkpoint = "t5-base"
+
+
+def preprocess_function(examples):
+    inputs = [prefix + doc for doc in examples["article"]]
+    model_inputs = tokenizer(inputs, max_length=1024, truncation=True)
+
+    labels = tokenizer(text_target=examples["abstract"], max_length=128, truncation=True)
+
+    model_inputs["labels"] = labels["input_ids"]
+    return model_inputs
+
+
 billsum = load_dataset("scientific_papers", "arxiv")
 
 tokenizer = AutoTokenizer.from_pretrained("t5-small")
 
-prefix = "summarize: "
+tokenized_billsum = billsum.map(preprocess_function, batched=True)
+
+model = AutoModelForSeq2SeqLM.from_pretrained(model_checkpoint)
+
+data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=model)
